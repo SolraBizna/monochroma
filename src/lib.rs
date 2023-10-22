65d63c148643106762fd65be481a611b22849db5
Solra Bizna <solra@bizna.name>
2023-10-22 15:26:52 -0600

    Document feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+/**
+ * This is a crate for processing and displaying monochrome bitmapped graphics.
+ *
+ * There are some optional features that are disabled by default:
+ *
+ * - `font`: Bitmapped font support. (Currently only Macintosh format)
+ * - `netpbm`: netpbm image input and output (pbm, pgm, ppm).
+ */
 mod display;
+#[doc(inline)]
 pub use display::Display;
 
 mod bitmap;
